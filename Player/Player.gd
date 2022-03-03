c606357f9e1fdfab4be3da47273be711e77b59a7
Jessica <0157862@schoolsnet.act.edu.au>
2022-03-03 11:48:20 +1100

    walls

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,21 +2,20 @@ extends KinematicBody2D
 
 var movement_speed = 200
 var bulletSource = preload ("res://Bullet/Bullet.tscn")
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
 	set_physics_process(true)
+	$Area2D.connect("area_entered", self, "_colliding")
+	$Area2D.connect("area_exited", self, "_not_colliding")
+
 
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		move_and_collide(Vector2(-movement_speed * delta, 0))
-	if Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right"):
 		move_and_collide(Vector2(movement_speed * delta, 0))
 
 func _process(delta):
@@ -25,3 +24,13 @@ func _process(delta):
 		bulletInstance.position = Vector2(position.x, position.y-20)
 		get_tree().get_root().add_child(bulletInstance)
 		
+func _colliding(area):
+	if area.is_in_group("left"):
+		print("left")
+		position.x=50
+	if area.is_in_group("right"):
+		print("right")
+		position.x=1230
+	
+func _not_colliding(area):
+	pass
